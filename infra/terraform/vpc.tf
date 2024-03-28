6465b4fe82aaec688160f933e597b3d14e3f7d18
igorferrati <ferrati.igor@gmail.com>
2024-03-28 17:26:43 -0300

    alterações

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -4,7 +4,7 @@ resource "google_compute_firewall" "webAccess" {
 
   allow {
     protocol = "tcp"
-    ports = ["5432", "54321", "8080", "8081", "3000", "3001","22"]
+    ports = ["5432", "54321", "80","8080", "22"]
   }
 
   source_ranges = ["0.0.0.0/0"]
