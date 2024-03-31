3009bb500e7f3e08f46f1d02d3113cfdf433ce60
igorferrati <ferrati.igor@gmail.com>
2024-03-31 14:36:46 -0300

    conn string

--- back/api-rest-go/database/connection.go
+++ back/api-rest-go/database/connection.go
@@ -15,7 +15,7 @@ var (
 )
 
 func ConectaDB() {
-	stringDeConexao := "host=cb889jp6h2eccm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com user=u1li0tmtiqjeil password=p43e27b856a96f68380a75833e95a79ec77cecd05dc7f5513d194f003cc47e4ff dbname=d36gsljaqq3o2e port=5432 sslmode=disable"
+	stringDeConexao := "host=localhost user=admin password=admin123 dbname=base_escola port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
